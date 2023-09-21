28bc0553331e5843b8961da3e7b723936df08cb2
Pinki Singh <web.pinkisingh@gmail.com>
2023-09-21 18:35:59 +0530

    removed preloader

--- Portfolio2/app.js
+++ Portfolio2/app.js
@@ -2,27 +2,27 @@
 let open = document.querySelector(".fa-bars-staggered");
 let close = document.querySelector(".fa-xmark");
 let ulContainer = document.querySelector(".ul-container");
-let loader = document.querySelector(".one");
+let preloader = document.querySelector(".one");
 
 // Media Query
 let mediaQuery = window.matchMedia("(max-width: 769px)");
 function handleMediaQuery(e) {
   if (e.matches) {
-    // gsap.from("nav" ,{
-    //     y: -300,
-    //     duration:1,
-    //  });
-    //  gsap.from(".section-1 img",{
-    //     x: 1200,
-    //     duration:2,
-    // });
-    // gsap.from(".section-1 p",{
-    //     x: -600,
-    //     duration:2,
-    // });
-    // gsap.from("nav h2",{
-    //     y: -200,
-    // })
+    gsap.from("nav" ,{
+        y: -300,
+        duration:1,
+     });
+     gsap.from(".section-1 img",{
+        x: 1200,
+        duration:2,
+    });
+    gsap.from(".section-1 p",{
+        x: -600,
+        duration:2,
+    });
+    gsap.from("nav h2",{
+        y: -200,
+    })
 
     loader.style.display = "none";
 
@@ -54,54 +54,46 @@ function handleMediaQuery(e) {
 mediaQuery.addEventListener("change", handleMediaQuery);
 handleMediaQuery(mediaQuery);
 
-// ANIMATION USING GSAP
+
+
 
 let mediaQuery2 = window.matchMedia("(min-width: 770px)");
 function anime(e) {
   if (e.matches) {
-       gsap.to('.one', {
-          y: -900,
-          ease:Expo.easeOut,
-          duration:2,
-          delay: 6,
-       })
-       gsap.to('.highlight',{
-        x:1300,
-        duration:2,
-        delay: 4,
-       })
-    gsap.from(".head-para-skill-name, .all-skill-name-box, .skill-name", {
-        opacity: 0,
-        duration: 2,
-        stagger: 0.3,
-        scrollTrigger: {
-          trigger: ".head-para-skill-name",
-          scroller: "body",
-          start: "top 100%",
-          end: "top 130%",
-        },
-      });
+
+
+    // page-2 animation
+    // gsap.from(".head-para-skill-name, .all-skill-name-box, .skill-name",{
+    //     opacity: 0,
+    //     duration:1,
+    //     stagger: 0.2,
+    //     scrollTrigger: {
+    //       trigger: ".head-para-skill-name",
+    //       scroller: "body",
+    //       start: "top 100%",
+    //       end: "top 130%",
+    //     },
+    //   });
 
     const id = setInterval(() => {
       gsap.from("nav li", {
         y: -300,
         stagger: 0.3,
-        duration: 1,
+        duration:1,
       });
       gsap.from(".section-1 img", {
         x: 1200,
-        duration: 2.3,
+        duration: 2,
       });
       gsap.from(".section-1 p", {
         x: -600,
-        duration: 2.3,
-        stagger: 0.3,
+        duration: 2,
       });
       gsap.from("nav h2", {
         y: -200,
       });
       clearInterval(id);
-    }, 5000);
+    }, 10);
   }
 }
 mediaQuery2.addEventListener("change", anime);
